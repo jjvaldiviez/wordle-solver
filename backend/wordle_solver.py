d24a47eabef14762b1590e38f758e5aa5b945e5c
Jacob Valdiviez <jacob.valdiviez@gm.com>
2025-07-18 15:28:00 -0400

    fully working now

--- backend/wordle_solver.py
+++ backend/wordle_solver.py
@@ -4,10 +4,22 @@ def get_guesses(grn, yel, gry):
     print("Converting guesses")
     grn = convert_grn(grn)
     yel = convert_yel(yel)
+    gry = convert_gry(gry, grn, yel)
     print(f"Greens: {grn}\nYellows: {yel}\nGrays: {gry}\n")
     print("Retrieving Guesses\n")
     options = get_word_options(grn, yel, gry)
     return [v for v in options if len(v) == 5]
+
+def convert_gry(gry, grn, yel):
+    ret = gry[:]
+    for i in range(len(gry)):
+        if gry[i] in grn:
+            ret.remove(gry[i])
+    for i in range(len(gry)):
+        for y in yel:
+            if gry[i] in y:
+                ret.remove(gry[i])
+    return ret
 
 def convert_yel(yel):
     ret = [[],[],[],[],[]]
